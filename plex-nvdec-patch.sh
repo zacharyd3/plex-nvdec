375cfdaeab951446c220127b29f0f70057882afc
zacharyd3 <zacharyd3@hotmail.com>
2019-04-02 23:34:29 -0400

    Added some comments to help make sense of the code

--- plex-nvdec-patch.sh
+++ plex-nvdec-patch.sh
@@ -8,7 +8,6 @@ docker cp $con:/usr/lib/plexmediaserver/Plex\ Transcoder "/mnt/user/Storage/Goog
 
 #Check if the files already been patched.
 pcheck="$(tail -n 1 '/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder')"
-
 echo ""
 #echo $pcheck
 patched=##patched
@@ -21,8 +20,10 @@ else
 	echo "<hr>"
 fi
 
+#Copy the default transcoder as a backup
 mv "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder" "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder2"
 
+#Adds the needed script to use hardware decoding
 cat > "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder" <<< '#!/bin/bash
 marap=$(cut -c 10-14 <<<"$@")
 if [ $marap <> "mpeg4" ]; then
@@ -33,12 +34,11 @@ fi
 
 ##patched'
 
+#Gives the files the correct permissions and copies them back to the docker container
 chmod +x "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder"
 chmod +x "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder2"
-
 docker cp "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder" $con:/usr/lib/plexmediaserver/Plex\ Transcoder
 docker cp "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder2" $con:/usr/lib/plexmediaserver/Plex\ Transcoder2
-
 docker exec -i $con chmod +x "/usr/lib/plexmediaserver/Plex Transcoder"
 docker exec -i $con chmod +x "/usr/lib/plexmediaserver/Plex Transcoder2"
 
