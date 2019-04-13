d30f586dd83363cc3529994490d8579e0d259b57
zacharyd3 <zacharyd3@hotmail.com>
2019-04-13 18:58:31 -0400

    Updated to better handle MPEG4 content

--- plex-nvdec-patch.sh
+++ plex-nvdec-patch.sh
@@ -6,6 +6,9 @@ con="$(docker ps -aqf 'name=PlexMediaServer')"
 #Copy the files needed from Plex.
 docker cp $con:/usr/lib/plexmediaserver/Plex\ Transcoder "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder"
 
+#Make a backup of the original
+docker cp $con:/usr/lib/plexmediaserver/Plex\ Transcoder "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder.bak"
+
 #Check if the files already been patched.
 pcheck="$(tail -n 1 '/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder')"
 echo ""
@@ -27,17 +30,16 @@ mv "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder" "/mn
 #Adds the needed script to use hardware decoding
 cat > "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder" <<< '#!/bin/bash
 marap=$(cut -c 10-14 <<<"$@")
-if [ $marap <> "mpeg4" ]; then
-     exec /usr/lib/plexmediaserver/Plex\ Transcoder2 -hwaccel nvdec "$@"
-else
+nvdec=$(nvidia-smi -q | grep -iq Decoder ; echo $?)
+if [[ "$marap" == "mpeg4" || $nvdec -ne 0 ]]; then
      exec /usr/lib/plexmediaserver/Plex\ Transcoder2 "$@"
+else
+     exec /usr/lib/plexmediaserver/Plex\ Transcoder2 -hwaccel nvdec "$@"
 fi
 
 ##patched'
 
 #Gives the files the correct permissions and copies them back to the docker container
-chmod +x "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder"
-chmod +x "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder2"
 docker cp "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder" $con:/usr/lib/plexmediaserver/Plex\ Transcoder
 docker cp "/mnt/user/Storage/Google Drive/Server Files/Plex Patch/Plex Transcoder2" $con:/usr/lib/plexmediaserver/Plex\ Transcoder2
 docker exec -i $con chmod +x "/usr/lib/plexmediaserver/Plex Transcoder"
